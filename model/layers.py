af256e88deed89b5ada4580601ce945e771d983a
fan1997 <2479031300@qq.com>
2021-02-05 21:47:19 +0800

    Update layers.py

--- model/layers.py
+++ model/layers.py
@@ -34,11 +34,8 @@ class Down(nn.Module):
         x = self.maxpool_conv(x)
         x = self.double_conv(x)
         return x
-
-
+"""
 class Up(nn.Module):
-    """Upscaling then double conv"""
-
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
         self.double_conv = DoubleConv(
@@ -55,29 +52,27 @@ class Up(nn.Module):
         x = torch.cat((x1, x2), dim=1)
         x = self.double_conv(x)
         return x
+"""
 
-# class Up(nn.Module):
-#     """Upscaling then double conv"""
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+        self.double_conv = DoubleConv(
+            in_channels, out_channels, in_channels // 2)
+        self.up = nn.ConvTranspose2d(
+            in_channels, in_channels // 2, kernel_size=2, stride=2, bias=True)
+        self.relu = nn.ReLU()
 
-#     def __init__(self, in_channels, out_channels, bilinear=True):
-#         super().__init__()
-#         self.double_conv = DoubleConv(
-#             in_channels, out_channels, in_channels // 2)
-#         self.up = nn.ConvTranspose2d(
-#             in_channels, in_channels // 2, kernel_size=2, stride=2, bias=True)
-#         self.relu = nn.ReLU()
-
-#     def forward(self, x1, x2):
-#         x1 = self.up(x1)
-#         x1 = self.relu(x1)
-#         diffY = x1.size()[2] - x2.size()[2]
-#         diffX = x1.size()[3] - x2.size()[3]  
-
-#         x2 = F.pad(x2, [diffX // 2, diffX - diffX // 2,
-#                         diffY // 2, diffY - diffY // 2])
-#         x = torch.cat((x1, x2), dim=1)
-#         x = self.double_conv(x)
-#         return x
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x1 = self.relu(x1)
+        diffY = x1.size()[2] - x2.size()[2]
+        diffX = x1.size()[3] - x2.size()[3]  
+        x2 = F.pad(x2, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat((x1, x2), dim=1)
+        x = self.double_conv(x)
+        return x
     
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
